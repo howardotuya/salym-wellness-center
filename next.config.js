587e7ab27933188a27ff45a568c7e05e52827d2e
howard otuya <howardotuya17@gmail.com>
2023-05-17 10:59:12 +0100

    Edited Cors xxnmn

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
     eslint: {
       // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
+      // your project has ESLint errors. 
       ignoreDuringBuilds: true,
     },
   };
